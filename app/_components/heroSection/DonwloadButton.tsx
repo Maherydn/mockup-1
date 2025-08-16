109927c05e51e2c33a920e3f13ea70202b9594a1
Daniel <maherydn@gmail.com>
2025-08-16 16:16:06 +0200

    (2)

--- app/_components/heroSection/DonwloadButton.tsx
+++ app/_components/heroSection/DonwloadButton.tsx
@@ -1,7 +1,16 @@
 import { BottomIcon } from "@/app/_assets/icon";
 
+const downloadCV = () => {
+    const link = document.createElement("a");
+    link.href = "/CV.pdf";
+    link.download = "CV.pdf";
+    document.body.appendChild(link);
+    link.click();
+    document.body.removeChild(link);
+  };
+
 export const DownloadButton = () => (
-  <button className="capitalize bg-yellow lg:h-14 lg:w-64 h-10 w-52 rounded-lg flex justify-evenly items-center hover:bg-yellow-300/50 duration-300 cursor-pointer mx-auto">
+  <button onClick={downloadCV} className="capitalize bg-yellow lg:h-14 lg:w-64 h-10 w-52 rounded-lg flex justify-evenly items-center hover:bg-yellow-300 duration-300 cursor-pointer mx-auto">
     <p className="lg:text-xl text-lg text-slate-600 font-bold">
       download my CV
     </p>
