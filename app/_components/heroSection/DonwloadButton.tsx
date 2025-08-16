46f64b1ff4a6cdcb722bec261e59b8096c1d325b
Daniel <maherydn@gmail.com>
2025-08-16 16:23:20 +0200

    debug CV to cv

--- app/_components/heroSection/DonwloadButton.tsx
+++ app/_components/heroSection/DonwloadButton.tsx
@@ -2,8 +2,8 @@ import { BottomIcon } from "@/app/_assets/icon";
 
 const downloadCV = () => {
     const link = document.createElement("a");
-    link.href = "/CV.pdf";
-    link.download = "CV.pdf";
+    link.href = "/cv.pdf";
+    link.download = "cv.pdf";
     document.body.appendChild(link);
     link.click();
     document.body.removeChild(link);
